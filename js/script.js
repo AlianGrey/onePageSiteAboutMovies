0b686634391471223b44a9bc33f1cafa488aa002
AlianGrey <ek371116@gmail.com>
2023-06-23 12:48:09 +0400

    replace html tags

--- js/script.js
+++ js/script.js
@@ -20,7 +20,50 @@ const movieDB = {
         "Лига справедливости",
         "Ла-ла лэнд",
         "Одержимость",
-        "Скотт Пилигрим против..."
+        "Скотт Пилигрим против...",
+        "А Новый фильм"
     ]
 };
+// 1)
+const adv = document.querySelectorAll('.promo__adv img'),
+    promo = document.querySelector('.promo__bg'),
+    genre = promo.querySelector('.promo__genre'),
+    movieList = document.querySelector('.promo__interactive-list');
+
+//console.dir(adv);
+//adv[0].remove();
+//adv[0].innerHTML = "";
+adv.forEach(item => {
+    item.remove();
+})
+
+// 2)
+//genre.innerHTML = 'ДРАМА';
+genre.textContent = 'ДРАМА';
+
+// 3)
+promo.style.backgroundImage = 'url("../img/bg.jpg")';
+//console.dir(bg);
+
+// 4)   &  5)
+//console.dir(movieList);
+
+movieDB.movies.sort();
+//console.log(movieDB.movies);
+movieList.innerHTML = '';
+console.log(movieList);
+
+movieDB.movies.forEach((item, key) => {
+    /*     let li = document.createElement('li');
+        li.classList.add('promo__interactive-item');
+        //console.log(item);
+        li.innerHTML = key + 1 + '.  ' + item;
+        movieList.append(li); */
+
+    movieList.innerHTML += `
+            <li class="promo__interactive-item"> ${key + 1}. ${item}
+            <div class="delete"></div>
+            </li>
+        `;
+})
 
